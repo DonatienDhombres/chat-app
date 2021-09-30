317f3539dc5ae3313568467be935385ab2ca9024
Donatien Dhombres <donatien.dbr@gmail.com>
2021-09-30 18:29:37 +0200

    Initial commit

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyBkMtax4b9DxII5p-kWXDUynHG3TTJHEZI',
+  authDomain: 'chat-web-app-f77ab.firebaseapp.com',
+  projectId: 'chat-web-app-f77ab',
+  storageBucket: 'chat-web-app-f77ab.appspot.com',
+  messagingSenderId: '575509089782',
+  appId: '1:575509089782:web:10373d0cf74916d9cd8bd7',
+};
+
+const app = firebase.initializeApp(config);
