4d67fdb5faaee5ceb0ab1a956cbecdde87d45e0f
Donatien Dhombres <donatien.dbr@gmail.com>
2021-10-27 16:01:29 +0200

    Created chat bottom ; send messages to the chat

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -1,7 +1,89 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { Alert, Icon, Input, InputGroup } from 'rsuite';
+import firebase from 'firebase/app';
+import { useParams } from 'react-router';
+import { useProfile } from '../../../context/profile.context';
+import { database } from '../../../misc/firebase';
+
+function assembleMessage(profile, chatId) {
+  return {
+    roomId: chatId,
+    author: {
+      name: profile.name,
+      uid: profile.uid,
+      createdAt: profile.createdAt,
+      ...(profile.avatar ? { avatar: profile.avatar } : {}),
+    },
+    createdAt: firebase.database.ServerValue.TIMESTAMP,
+  };
+}
 
 const Bottom = () => {
-  return <div>bottom</div>;
+  const [input, setInput] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const { chatId } = useParams();
+  const { profile } = useProfile();
+
+  const onInputChange = useCallback(value => {
+    setInput(value);
+  }, []);
+
+  const onSendClick = async () => {
+    if (input.trim() === '') {
+      // eslint-disable-next-line no-useless-return
+      return;
+    }
+
+    const msgData = assembleMessage(profile, chatId);
+    msgData.text = input;
+
+    /* Create a new message */
+    const messageId = database.ref('messages').push().key;
+
+    /* Create the updates array */
+    const updates = {};
+    updates[`/messages/${messageId}`] = msgData;
+    updates[`/rooms/${chatId}/lastMessage`] = { ...msgData, msgId: messageId };
+
+    setIsLoading(true);
+
+    try {
+      await database.ref().update(updates);
+      setInput('');
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      Alert.error(err.message);
+    }
+  };
+
+  const onKeyDownHandler = ev => {
+    if (ev.keyCode === 13) {
+      ev.preventDefault();
+      onSendClick();
+    }
+  };
+
+  return (
+    <div>
+      <InputGroup>
+        <Input
+          placeholder="Write a new message here..."
+          value={input}
+          onChange={onInputChange}
+          onKeyDown={onKeyDownHandler}
+        />
+        <InputGroup.Button
+          color="blue"
+          appearance="primary"
+          onClick={onSendClick}
+          disabled={isLoading}
+        >
+          <Icon icon="send" />
+        </InputGroup.Button>
+      </InputGroup>
+    </div>
+  );
 };
 
 export default Bottom;
