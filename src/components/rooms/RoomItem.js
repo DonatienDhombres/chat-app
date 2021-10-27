76e0c7e06f1656be69edef3747c930c7da452aba
Donatien Dhombres <donatien.dbr@gmail.com>
2021-10-27 16:24:11 +0200

    Updated Room Item to display last message

--- src/components/rooms/RoomItem.js
+++ src/components/rooms/RoomItem.js
@@ -1,22 +1,44 @@
 import React from 'react';
 import TimeAgo from 'timeago-react';
+import ProfileAvatar from '../ProfileAvatar';
 
 const RoomItem = ({ room }) => {
-  const { createdAt, name } = room;
+  const { createdAt, name, lastMessage } = room;
   return (
     <div>
       <div className="d-flex justify-content-between align-items-center">
         <h3 className="test-disappear">{name}</h3>
         <span>
           <TimeAgo
-            datetime={new Date(createdAt)}
+            datetime={
+              lastMessage
+                ? new Date(lastMessage.createdAt)
+                : new Date(createdAt)
+            }
             className="font-normal text-black-45"
           />
         </span>
       </div>
 
       <div className="d-flex align-items-center text-black-70">
-        <span>No message yet</span>
+        {lastMessage ? (
+          <>
+            <div className="d-flex align-items-center">
+              <ProfileAvatar
+                src={lastMessage.author.avatar}
+                name={lastMessage.author.name}
+                size="sm"
+              />
+            </div>
+
+            <div className="text-disappear ml-2">
+              <div className="italic">{lastMessage.author.name}</div>
+              <span>{lastMessage.text}</span>
+            </div>
+          </>
+        ) : (
+          <span>No message yet</span>
+        )}
       </div>
     </div>
   );
