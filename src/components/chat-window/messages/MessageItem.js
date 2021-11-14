283ff0d45c2b853f8e2f3ceb1230e40d5978a49f
Donatien Dhombres <donatien.dbr@gmail.com>
2021-11-14 22:55:36 +0100

    Handle delete message

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -9,7 +9,7 @@ import { auth } from '../../../misc/firebase';
 import { useHover, useMediaQuery } from '../../../misc/custom-hooks';
 import IconBtnControl from './IconBtnControl';
 
-const MessageItem = ({ message, handleAdmin, handleLike }) => {
+const MessageItem = ({ message, handleAdmin, handleLike, handleDelete }) => {
   const { author, createdAt, text, likes, likeCount } = message;
 
   const [selfRef, isHovered] = useHover();
@@ -32,7 +32,6 @@ const MessageItem = ({ message, handleAdmin, handleLike }) => {
     >
       <div className="d-flex align-items-center font-bolder mb-1">
         <PresenceDot uid={author.uid} />
-
         <ProfileAvatar
           src={author.avatar}
           name={author.name}
@@ -52,7 +51,6 @@ const MessageItem = ({ message, handleAdmin, handleLike }) => {
             </Button>
           )}
         </ProfileInfoBtnModal>
-
         <TimeAgo
           datetime={createdAt}
           className="font-normal text-black-45 ml-2"
@@ -65,6 +63,14 @@ const MessageItem = ({ message, handleAdmin, handleLike }) => {
           onClick={() => handleLike(message.id)}
           badgeContent={likeCount}
         />
+        {isAuthor && (
+          <IconBtnControl
+            isVisible={canShowIcons}
+            iconName="close"
+            tooltip="Delete this message"
+            onClick={() => handleDelete(message.id)}
+          />
+        )}
       </div>
       <div>
         <span className="word-break-all">{text}</span>
--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -67,11 +67,9 @@ const Messages = () => {
           alertMsg = 'Like removed';
         } else {
           msg.likeCount += 1;
-
           if (!msg.likes) {
             msg.likes = {};
           }
-
           msg.likes[uid] = true;
           alertMsg = 'Like added';
         }
@@ -83,6 +81,41 @@ const Messages = () => {
     Alert.info(alertMsg, 4000);
   }, []);
 
+  const handleDelete = useCallback(
+    async msgId => {
+      // eslint-disable-next-line no-alert
+      if (!window.confirm('Delete this message?')) {
+        return;
+      }
+
+      const isLast = messages[messages.length - 1].id === msgId;
+
+      const updates = {};
+
+      updates[`/messages/${msgId}`] = null;
+
+      if (isLast && messages.length > 1) {
+        updates[`/rooms/${chatId}/lastMessage`] = {
+          ...messages[messages.length - 2],
+          msgId: messages[messages.length - 2].id,
+        };
+      }
+
+      if (isLast && messages.length === 1) {
+        updates[`/rooms/${chatId}/lastMessage`] = null;
+      }
+
+      try {
+        await database.ref().update(updates);
+
+        Alert.info('Message has been deleted');
+      } catch (err) {
+        Alert.error(err.message);
+      }
+    },
+    [chatId, messages]
+  );
+
   return (
     <ul className="msg-list custom-scroll">
       {isChatEmpty && <li>No messages yet</li>}
@@ -93,6 +126,7 @@ const Messages = () => {
             message={msg}
             handleAdmin={handleAdmin}
             handleLike={handleLike}
+            handleDelete={handleDelete}
           />
         ))}
     </ul>
